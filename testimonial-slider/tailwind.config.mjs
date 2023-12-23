42ba1f6902e803e6acb3896e7b1850b17efa9404
Wanderlee Max <wanderleedev@gmail.com>
2023-12-22 23:35:59 -0500

    slider-testimonial

---
+++ testimonial-slider/tailwind.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'img-wrapper': "url('./src/assets/pattern-bg.svg')",
+				'wave' : "url('./src/assets/pattern-curve.svg')",
+				'pattern-quote': "url('./src/assets/pattern-quotes.svg')"
+			},
+			backgroundSize: {
+				'size-quote': '15%',
+				'size-wave': '60% 5rem',
+				'initial': 'initial'
+			}
+		},
+	},
+	plugins: [],
+}
